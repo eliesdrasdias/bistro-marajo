f14c8ad0860b378cd642f18df67d76f20a742b06
eliesdrasdias <a0xlotus@gmail.com>
2026-04-26 08:32:29 -0300

    feat: cria modelagem inicial do banco de dados do cardapio

---
+++ js/dados.js
@@ -0,0 +1,46 @@
+const cardapio = [
+    {
+        id: 1,
+        nome: 'Filé de Dourada a Milanesa com Vinagrete',
+        descricao: 'Empanada com farinha de rosca e panko.',
+        preco: 25.00,
+        turno: ['almoco'],
+        categoria: 'comida_normal',
+        imagem: 'assets/dourada-a-milanesa.jpg',
+        pratoDoDia: true
+    },
+
+    {
+        id: 2,
+        nome: 'Strogonoff de Alcatra',
+        descricao: 'Carne macia selada, em um molho cremoso à base de creme de leite, ketchup, mostarda e molho inglês',
+        preco: 25.00,
+        turno: ['almoco', 'jantar'],
+        categoria: 'comida_normal',
+        imagem: 'assets/strogonoff.jpg',
+        pratoDoDia: false
+    },
+
+    {
+        id: 3,
+        nome: 'Alcatra em Tiras com Fritas',
+        descricao: 'Carne macia, feita na chapa e acebolada',
+        preco: 30.00,
+        turno: ['almoco'],
+        categoria: 'comida_normal',
+        imagem: 'assets/alcatra.jpg',
+        pratoDoDia: false
+    },
+
+    {
+        id: 4,
+        nome: 'Sopa de Carne com Legumes',
+        descricao: 'Caldo leve com cenoura, batata e legumes diversos...',
+        preco: 15.00,
+        turno: ['jantar'],
+        categoria: 'caldo',
+        imagem: 'assets/sopa-de-carne.jpg',
+        pratoDoDia: false
+    },
+
+]
